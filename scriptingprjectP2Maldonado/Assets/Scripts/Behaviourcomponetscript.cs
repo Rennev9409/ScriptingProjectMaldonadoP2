cc2d1cc7729e1dc29c38ce47e686550efd607c39
Rennev9409 <Renemaldonado2010@icloud.com>
2024-09-27 08:38:23 -0700

    Update Behaviourcomponetscript.cs

--- scriptingprjectP2Maldonado/Assets/Scripts/Behaviourcomponetscript.cs
+++ scriptingprjectP2Maldonado/Assets/Scripts/Behaviourcomponetscript.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Behaviourcomponetscript : MonoBehaviour
@@ -7,12 +9,25 @@ public class Behaviourcomponetscript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+            { }
+        { }
+    If (Input).GetKeyDown(KeyCode.G))
+{
+
+GetComponents<Renderer>().material.color = Color.green;
+private object If(object input)
+    {
+        throw new NotImplementedException();
     }
 }
+
+  
